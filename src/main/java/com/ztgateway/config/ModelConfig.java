9a24cf575ec18787aff9d34b7c7ec64b2b3a3795
Sharanu Malipatil <malipatils249@gmail.com>
2026-06-16 13:55:44 +0530

    Update ModelConfig.java

--- src/main/java/com/ztgateway/config/ModelConfig.java
+++ src/main/java/com/ztgateway/config/ModelConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ModelConfig {
+public class ModelConfig {                                                                                                
 
     private static final Logger log = LoggerFactory.getLogger(ModelConfig.class);
 
